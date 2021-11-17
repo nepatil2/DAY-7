aadf915f97f925504606f65f1e890ecf8d76e3ff
nepatil2 <nehapatil22899@gmail.com>
2021-11-17 16:01:17 +0530

    Array_TwoArray

---
+++ TwoArray.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+for ((i=0; i<100; i++))
+do
+	arr[$i]=$i;
+done
+	echo "array = " ${arr[*]};
+
+for (( i=1; i<${#arr[*]}; i++ ))
+do
+	if [ $((arr[i]%11)) -eq 0 ]
+	then
+		twinArr[$i]=$i;
+	fi
+done
+	echo "Twin Numbers in array: " ${twinArr[*]};
